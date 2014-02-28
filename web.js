97619de3a736eb8cad1dfc9cfd23756234c638f2
Niklas Waller <niklas.waller@gmail.com>
2014-02-28 11:22:21 +0100

    sadsa

--- web.js
+++ web.js
@@ -6,10 +6,10 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello World 2!');
 });
 
-var port = Number(process.env.PORT || 80);
+var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
